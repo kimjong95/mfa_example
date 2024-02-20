64425ded9169918437326f7866950affebbe870b
kimjong95 <mje807@naver.com>
2024-02-20 09:07:06 +0900

    ui-kit 설정

---
+++ packages/ui-kit/src/index.ts
@@ -0,0 +1,4 @@
+import Button from "./components/Button";
+import "./global.css";
+
+export { Button };
---
+++ packages/ui-kit/vite.config.ts
@@ -0,0 +1,30 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [react(), dts({ insertTypesEntry: true })],
+  build: {
+    outDir: "./dist",
+    lib: {
+      entry: ["src/index.ts"],
+      name: "ui-kit",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "style.css") {
+            return "index.css";
+          }
+          return assetInfo.name || "";
+        },
+      },
+    },
+  },
+});

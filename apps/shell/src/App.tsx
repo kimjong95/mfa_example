38393a57e05b05ea2fdab5bce9b552c9138728b1
kimjong95 <mje807@naver.com>
2024-02-25 21:46:54 +0900

    shell-router 만들기

--- apps/shell/src/App.tsx
+++ apps/shell/src/App.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { test } from "@career-up/shell-router";
 import { Button, Icon } from "@career-up/ui-kit";
 import "@career-up/ui-kit/index.css";
 
-const App = () => (
-  <div className="container">
-    <div>Name: shell</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Empty CSS</div>
-    <div>
-      <Button>Hello</Button>
+test();
+
+const App = () => {
+  return (
+    <div className="container">
+      <div>Name: shell</div>
+      <div>Framework: react</div>
+      <div>Language: TypeScript</div>
+      <div>CSS: Empty CSS</div>
+      <div>
+        <Button>Hello</Button>
+      </div>
+      <div>
+        <Icon.Briefcase />
+      </div>
     </div>
-    <div>
-      <Icon.Briefcase />
-    </div>
-  </div>
-);
+  );
+};
 const rootElement = document.getElementById("app");
 if (!rootElement) throw new Error("Failed to find the root element");
 

4b31d54fbde871bb258342b89e28135b3e05ea28
kimjong95 <mje807@naver.com>
2024-03-06 08:00:25 +0900

    채용서비스 작업완료

--- apps/shell/src/router.tsx
+++ apps/shell/src/router.tsx
@@ -8,6 +8,7 @@ import Layout from "./components/Layout";
 import { Auth0ProviderWithNavigator } from "./components/auto0-provider-with-navigator";
 import {
   appEduBasename,
+  appJobBasename,
   appNetworkBasename,
   appPostingBasename,
 } from "./constants/prefix";
@@ -15,6 +16,7 @@ import {
 const AppPostingLazy = React.lazy(() => import("./components/app-posting"));
 const AppEduLazy = React.lazy(() => import("./components/app-edu"));
 const AppNetworkLazy = React.lazy(() => import("./components/app-network"));
+const AppJobLazy = React.lazy(() => import("./components/app-job"));
 
 const browserRouter = createBrowserRouter([
   {
@@ -50,6 +52,14 @@ const browserRouter = createBrowserRouter([
         element: (
           <Suspense fallback="Loading Network...">
             <AppNetworkLazy />
+          </Suspense>
+        ),
+      },
+      {
+        path: `${appJobBasename}/*`,
+        element: (
+          <Suspense fallback="Loading Job...">
+            <AppJobLazy />
           </Suspense>
         ),
       },
